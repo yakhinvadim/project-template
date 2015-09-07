57dd305ebfb15ae00e080e20821f7a87638e48e4
yakhinvadim <yakhin.v@gmail.com>
2015-09-07 18:48:54 +0300

    add postcss

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,20 @@
 'use strict';
 
-var gulp = require('gulp');
+var gulp    = require('gulp'),
+    postcss = require('postcss');
 
+// default
 gulp.task('default', function() {
+});
+
+
+// postcss
+gulp.task('css', function() {
+
+  var processors = [];
+
+  return gulp.src( 'src/css/*.css')
+         .pipe( postcss(processors) )
+         .pipe( gulp.dest('dest/css'));
+
 });
