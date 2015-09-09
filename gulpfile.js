59e7a8c4ddde1470390804a7e7e77180e2d11596
yakhinvadim <yakhin.v@gmail.com>
2015-09-09 13:15:17 +0300

    add task icons

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 'use strict';
 
 var gulp         = require('gulp'),
+    svgstore     = require('gulp-svgstore'),
+    svgmin       = require('gulp-svgmin'),
     postcss      = require('gulp-postcss'),
     precss       = require('precss'),
-    autoprefixer = require('autoprefixer');
+    autoprefixer = require('autoprefixer'),
+    path         = require('path');
 
 
 // default
@@ -23,5 +26,28 @@ gulp.task( 'css', function() {
   return gulp.src( 'src/css/*.css' )
     .pipe( postcss( processors ))
     .pipe( gulp.dest('dest/css') );
+
+});
+
+
+// svg
+
+gulp.task('icons', function() {
+
+  return gulp
+    .src('src/img/icons/*.svg')
+    .pipe(svgmin(function(file) {
+      var prefix = path.basename(file.relative, path.extname(file.relative));
+      return {
+        plugins: [{
+          cleanupIDs: {
+            prefix: prefix + '-',
+            minify: true
+          }
+        }]
+      };
+    }))
+    .pipe(svgstore())
+    .pipe(gulp.dest('dest/img'));
 
 });
