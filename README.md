24f5fd6a74765fc87c1ab094a60e387011408c86
Sava <ekaterina.kozlova@kodix.ru>
2015-09-15 13:01:52 +0300

    grid

--- README.md
+++ README.md
@@ -13,3 +13,5 @@ compile html | `gulp html`
 build css | `gulp css`
 build svg sprite | `gulp icons`
 watch changes | `gulp watch`
+clean dest folder | `gulp clean`
+run localhost:9080 | `static-server dest`
--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,21 @@
 
 var gulp         = require('gulp'),
     watch        = require('gulp-watch'),
+    plumber      = require('gulp-plumber'),
     jade         = require('gulp-jade'),
     svgstore     = require('gulp-svgstore'),
     svgmin       = require('gulp-svgmin'),
     postcss      = require('gulp-postcss'),
+    sourcemaps   = require('gulp-sourcemaps'),
     precss       = require('precss'),
     autoprefixer = require('autoprefixer'),
-    path         = require('path');
+    calc         = require('postcss-calc'),
+    path         = require('path'),
+    del          = require('del'),
+    concatCss    = require('gulp-concat-css');
 
 var paths = {
-  jade: 'src/*.jade',
+  jade: 'src/**/*.jade',
   css: 'src/css/**/*.css',
   js: 'src/js/**/*.js',
   img: 'src/img/*',
@@ -31,12 +36,17 @@ gulp.task( 'css', function() {
 
   var processors = [
     autoprefixer({ browsers: ['> 0.15% in RU'] }),
-    precss()
+    precss(),
+    calc()
   ];
 
-  return gulp.src( 'src/css/*.css' )
-    .pipe( postcss( processors ))
-    .pipe( gulp.dest('dest/css') );
+  gulp.src( 'src/css/*.css' )
+  .pipe(plumber())
+  // .pipe(concatCss('style.css'))
+  .pipe(sourcemaps.init())
+  .pipe(postcss(processors))
+  .pipe(sourcemaps.write('.'))
+  .pipe( gulp.dest('dest/css') );
 
 });
 
@@ -45,21 +55,20 @@ gulp.task( 'css', function() {
 
 gulp.task('icons', function() {
 
-  return gulp
-    .src('src/img/icons/*.svg')
-    .pipe(svgmin(function(file) {
-      var prefix = path.basename(file.relative, path.extname(file.relative));
-      return {
-        plugins: [{
-          cleanupIDs: {
-            prefix: prefix + '-',
-            minify: true
-          }
-        }]
-      };
-    }))
-    .pipe(svgstore())
-    .pipe(gulp.dest('dest/img'));
+  gulp.src('src/img/icons/*.svg')
+  .pipe(svgmin(function(file) {
+    var prefix = path.basename(file.relative, path.extname(file.relative));
+    return {
+      plugins: [{
+        cleanupIDs: {
+          prefix: prefix + '-',
+          minify: true
+        }
+      }]
+    };
+  }))
+  .pipe(svgstore())
+  .pipe(gulp.dest('dest/img'));
 
 });
 
@@ -68,9 +77,12 @@ gulp.task('icons', function() {
 
 gulp.task('html', function() {
 
-  gulp.src('src/*.jade')
-    .pipe(jade())
-    .pipe(gulp.dest('dest'));
+  gulp.src('src/**/*.jade')
+  .pipe(plumber())
+  .pipe(jade({
+    pretty: true
+  }))
+  .pipe(gulp.dest('dest'));
 
 });
 
@@ -82,5 +94,14 @@ gulp.task('watch', function() {
   watch(paths.css,   function() { gulp.start('css');   });
   watch(paths.jade,  function() { gulp.start('html');  });
   watch(paths.icons, function() { gulp.start('icons'); });
+
+});
+
+
+// clean
+
+gulp.task('clean', function() {
+
+  del('dest/**');
 
 });
